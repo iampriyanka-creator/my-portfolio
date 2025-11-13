9bd394c73453a63a07d785569c49038502c010ca
iampriyanka-creator <priyankamathematician2k24@gmail.com>
2025-11-13 16:59:55 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,55 @@
+
+
+
+
+// use console.log() to print message to the console.
+console.log("Hello, World! Let's Learn some JavaScript.");
+
+
+
+
+
+
+// 'Let' is used for variables whose value can change.
+Letage = 25;
+console.log("Initial age:", age); // output: initial age: 25
+age = 26; // the value can be uptaded.
+console.log("updated age:", age); // output: initial age: 26
+
+// 'const is used for constants, whose value cannot be changed once assigned.
+console. birthyear =1999;
+console.log("birth year:", birthyear); // output: birth year: 1999
+// birthyear = 2000; // this line would cause an error1 uncomment to see.
+
+console.log("player score:", score); // output: player score: 100
+
+console.log("\n--- End of variable Declaration ---\n");
+
+
+
+
+
+
+
+// String: Textual data, enclosed in quotes.
+const name = "Alice";
+console.log("Name (String):",);
+
+// Number: Integers or floating-point numbers.
+LetaccountBalanceb= 150.75;
+console.log("Balance (Number):", accountBalance);
+
+// Boooean: Represents true or false.
+LetisActive = true;
+console.log("Is Avtive? (Boolean):", isActive);
+
+// Object: A collection of key-value pairs.
+const person = {
+    firstName: "john",
+    LastName: "Doe",
+    age:  30
+};
+console.log("person (Object):", person);
+console.log("person's first name:", person.firstName);
+// Access properties with dot notation 
+// player score: 100
